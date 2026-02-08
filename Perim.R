64679eb533cf0f25af015af23cb88ccf7c44ee9d
mariajose monsonis <monllom3@alumni.uv.es>
2026-02-08 19:51:47 +0100

    Primer commit: archivos de áreas y perímetros

---
+++ Perim.R
@@ -0,0 +1,14 @@
+# Perimetro del cuadrado
+perimetro_cuadrado <- function(lado) {
+  4 * lado
+}
+
+# Longitud de circunferencia
+longitud_circunferencia <- function(radio) {
+  2 * pi * radio
+}
+
+# Perimetro del rectángulo
+perimetro_rectangulo <- function(largo, ancho) {
+  2 * (largo + ancho)
+}
